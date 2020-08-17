a3a90b0954cadab6406d625764115a78d5a54719
Bria White <bria.awhite@gmail.com>
2020-08-17 03:46:03 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -10,7 +10,7 @@ class BankAccount
     end
 
     def valid?
-       if self.status == "open" and self.balance > 0
+       if self.status == "open" && self.balance > 0
          true
        else
          false
